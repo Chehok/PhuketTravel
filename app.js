10e771b472abf66adee4e6786dd1b9994e0fabd5
Chehok <3070311@gmail.com>
2024-04-24 20:32:47 +0900

    로그인 화면, morgan 적용, user테이블 작성

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 const nunjucks = require('nunjucks');
 const { sequelize, Tourist } = require('./models');
 
 const userRouter = require('./routes/user');
+const transportationRouter = require('./routes/transportation');
 
 dotenv.config(); // .env 파일을 읽어서 process.env로 만든다. (dotenv => dot(.) + env) 비밀 키들을 관리하기 위함.
 
@@ -29,7 +31,7 @@ sequelize.sync({ force: false })
 app.use(
     // 'abcd',  <= 와 같이 첫번째 인자에 경로를 지정해줄 수 있다. 그럼 주소가 바뀜 (localhost:5000/abcd)
     // 지정해주지 않으면 '/'가 디폴트 값으로 지정되어있는걸로 처리.
-    // morgan('dev'), // 서버에 들어온 응답과 요청을 기록해주는 미들웨어, 기록 후 next 호출 // FIXME
+    morgan('dev'), // 서버에 들어온 응답과 요청을 기록해주는 미들웨어, 기록 후 next 호출
     express.static(path.join(__dirname, 'public')), // 요청하는 파일이 있을 때 파일 경로를 제공하며, localhost:5000/ 에 접속하면 public으로 경로를 바꿔줌
     express.json(), // put이나 patch, post 요청 시에 req.body에 프런트에서 온 데이터를 넣어줌
     express.urlencoded({ extended: false })
@@ -37,6 +39,7 @@ app.use(
 
 // 각 주소에 해당하는 라우터로 넘김
 app.use('/user', userRouter);
+app.use('/transportation', transportationRouter);
 
 app.use(async (req, res, next) => {
     try {
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const Tourist = require('./tourist');
+const User = require('./user');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env]; // 설정 로딩
@@ -14,11 +15,14 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 db.Tourist = Tourist;
+db.User = User;
 
 // init으로 sequelize와 연결
 Tourist.init(sequelize);
+User.init(sequelize);
 
 // associate로 관계 설정.
 Tourist.associate(db);
+User.associate(db);
 
 module.exports = db;
