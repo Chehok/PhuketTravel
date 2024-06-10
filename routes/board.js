2dd6f864c9fa47e4ddeb4e3394089303ed2daa97
Chehok <3070311@gmail.com>
2024-06-10 21:43:44 +0900

    route/board.js 주석 (풀이 설명)

--- routes/board.js
+++ routes/board.js
@@ -11,17 +11,15 @@ const router = express.Router();
 router.route('/')
     .get(async (req, res, next) => {
         try {
-            const boards = await Board.findAll();
-            res.render('board', {
-                title: require('../package.json').name,
+            const boards = await Board.findAll();   // DB 에서 게시글 전부 가져오기 / await 는 DB 에 접근할 때 무조건 사용해야 한다.
+            res.render('board', {                   // 'board' => board.html 을 렌더링 한다는 의미
+                title: require('../package.json').name,     // 내용물 -> title / port / boards 데이터를 board.html 에 같이 넣어서 보내준다.
                 port: process.env.PORT,
-                boards: boards.map(v => ({
-                    boardId: v.dataValues.boardId,
-                    title: v.dataValues.title,
+                boards: boards.map(v => ({                  // boards = [ { 게시판 번호1, 제목1, 작성일1 }, { 게시판 번호2, 제목2, 작성일2 }, { 게시판 번호3, 제목3, 작성일3 } ] 와 같이 여러개의 게시글이 들어있다.
+                    boardId: v.dataValues.boardId,          // boardId = 게시판 번호, title = 제목, createdAt = 작성일
+                    title: v.dataValues.title,              // nunjucks 를 이용하여 게시글(boards)의 갯수만큼 게시판 테이블을 그려준다.
                     createdAt:
-                        `${v.dataValues.createdAt.getFullYear()}-` +
-                        ((v.dataValues.createdAt.getMonth() + 1).toString().length == 1 ? `0${(v.dataValues.createdAt.getMonth() + 1)}-` : `${(v.dataValues.createdAt.getMonth() + 1)}-`) +
-                        (v.dataValues.createdAt.getDate().toString().length == 1 ? `0${(v.dataValues.createdAt.getDate())}` : `${(v.dataValues.createdAt.getDate())}`)
+                        `${v.dataValues.createdAt.getFullYear()}-${v.dataValues.createdAt.getMonth() + 1}-${v.dataValues.createdAt.getDate()}` // yyyy-MM-dd 형식으로 만듬. 어디서 긁어왔는지 기억안나요..
                 })),
             });
         } catch (err) {
@@ -82,9 +80,8 @@ router.route('/:boardId')
                         name: v.User ? v.User.dataValues.name : "",
                         guestId: v.dataValues.guestId,
                         comment: v.dataValues.comment,
-                        createdAt: `${v.dataValues.createdAt.getFullYear()}-` +
-                            ((v.dataValues.createdAt.getMonth() + 1).toString().length == 1 ? `0${(v.dataValues.createdAt.getMonth() + 1)}-` : `${(v.dataValues.createdAt.getMonth() + 1)}-`) +
-                            (v.dataValues.createdAt.getDate().toString().length == 1 ? `0${(v.dataValues.createdAt.getDate())}` : `${(v.dataValues.createdAt.getDate())}`)
+                        createdAt:
+                            `${v.dataValues.createdAt.getFullYear()}-${v.dataValues.createdAt.getMonth() + 1}-${v.dataValues.createdAt.getDate()}` // yyyy-MM-dd 형식으로 만듬. 어디서 긁어왔는지 기억안나요..
                     }))
                 });
             }
@@ -141,8 +138,8 @@ router.route('/update/:boardId')
             await Board.update({
                 title,
                 main
-            },{
-                where: {boardId}
+            }, {
+                where: { boardId }
             })
 
             res.redirect(`/menu/board/${boardId}`)
